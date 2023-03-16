6b183f1ed5cdce990a375bf3b955dc59f27f8ad5
kuma35 <sindobook@gmail.com>
2023-03-16 19:43:49 +0900

    mv build.sh to compile.bash, and mkdir in sedout, po, dst_dir.
    JP:build.shをやっぱり個人的に使い慣れた compile.bash に名称変更、
    bash スクリプトに変更(for pushd,popd)
    あらかじめ、 sedout, po, dst_dir(docs/html) の下に
    c3ref session syntax サブフォルダを生成するようにした。
    そうしないと、 po4a-update や po4a-tranlate がコケるので。

---
+++ compile.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+PROJ=${HOME}/work/sqlite-docs-ja
+SEDOUT_DIR=${PROJ}/sedout
+PO_DIR=${PROJ}/po
+DST_DIR=${PROJ}/docs/html
+C3REF_DIR=c3ref
+SESSION_DIR=session
+SYNTAX_DIR=syntax
+
+pushd ${PROJ}
+
+for target_dir in ${SEDOUT_DIR} ${PO_DIR} ${DST_DIR}
+do
+    pushd ${target_dir}
+    for dir_name in ${C3REF_DIR} ${SESSION_DIR} ${SYNTAX_DIR}
+    do
+	if [ ! -d ${dir_name} ]; then
+	    mkdir ${dir_name}
+	fi
+    done
+    popd
+done
+make
+exitcode=$?
+if [ ${exitcode} -ne 0 ]; then
+    notify-send -u critical sqlite-docs-ja "Makefile エラー"
+    exit ${exitcode}
+fi
+echo "restore htmls..."
+${PROJ}/restore-htmls.sh
+notify-send -u normal sqlite-docs-ja "compile完了。"
+
+popd
