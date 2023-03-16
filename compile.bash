c7e0497f62b8c19b82b3d714da9210cb61ff68e1
kuma35 <sindobook@gmail.com>
2023-03-17 03:46:22 +0900

    delete restore-htmls
    JP:git-docs-jaのように毎回全htmlを再作成する訳ではないので
    restore-htmls.shは必要ない。

--- compile.bash
+++ compile.bash
@@ -26,8 +26,6 @@ if [ ${exitcode} -ne 0 ]; then
     notify-send -u critical sqlite-docs-ja "Makefile エラー"
     exit ${exitcode}
 fi
-echo "restore htmls..."
-${PROJ}/restore-htmls.sh
 notify-send -u normal sqlite-docs-ja "compile完了。"
 
 popd
